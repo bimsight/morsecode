bd3ce27e4e362e78f7bd150717711db918346d32
bimsight <iliaterzi15@gmail.com>
2018-02-25 13:06:11 +0100

    Morse Code Converter

---
+++ MorseCode/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("**** Fun with Morse Code *****");
+
+            Console.Write("Input: ");
+            string input = Console.ReadLine().ToUpper();
+            Console.WriteLine("Output: ");
+
+            Dictionary<char, string> morse = new Dictionary<char, string>();
+            morse.Add('A', ".-");
+            morse.Add('B', "-...");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (morse.ContainsKey(c))
+                {
+                    Console.Write(morse[c]);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
