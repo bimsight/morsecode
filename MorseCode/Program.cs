95dcabea39b2491e73bb28281414b57d62672231
bimsight <iliaterzi15@gmail.com>
2018-02-25 16:01:33 +0100

    Morse Code Converter

--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -11,25 +11,62 @@ namespace MorseCode
         static void Main(string[] args)
         {
 
-            Console.WriteLine("**** Fun with Morse Code *****");
+            Console.WriteLine("**** Fun with Morse Code ****");
 
             Console.Write("Input: ");
             string input = Console.ReadLine().ToUpper();
-            Console.WriteLine("Output: ");
+            Console.WriteLine();
+            Console.Write("Output: ");
 
-            Dictionary<char, string> morse = new Dictionary<char, string>();
-            morse.Add('A', ".-");
-            morse.Add('B', "-...");
+            Dictionary<char, string> morse = new Dictionary<char, string>() {
+                {'A', ".-"},
+                {'B', "-..."},
+                {'C', "-.-."},
+                {'D', "-.."},
+                {'E', "."},
+                {'F', "..-."},
+                {'G', "--."},
+                {'H', "...."},
+                {'I', ".."},
+                {'J', ".---"},
+                {'K', "-.-"},
+                {'L', ".-.."},
+                {'M', "--"},
+                {'N', "-."},
+                {'O', "---"},
+                {'P', ".--."},
+                {'Q', "--.-"},
+                {'R', ".-."},
+                {'S', "..."},
+                {'T', "-"},
+                {'U', "..-"},
+                {'V', "...-"},
+                {'W', ".--"},
+                {'X', "-..-"},
+                {'Y', "-.--"},
+                {'Z', "--.."},
+                {'0', "-----"},
+                {'1', ".----"},
+                {'2', "..---"},
+                {'3', "...--"},
+                {'4', "....-"},
+                {'5', "....."},
+                {'6', "-...."},
+                {'7', "--..."},
+                {'8', "---.."},
+                {'9', "----."},
+            };
 
             for (int i = 0; i < input.Length; i++)
             {
+                
                 char c = input[i];
                 if (morse.ContainsKey(c))
                 {
                     Console.Write(morse[c]);
                 }
             }
-
+            Console.WriteLine();
             Console.ReadLine();
 
         }
