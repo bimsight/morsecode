361c729ce79de1c0559ddc594909a9644e12268b
Unknown <iliaterzi15@gmail.com>
2018-03-11 14:25:37 +0100

    moved Morse code encoding to a specialized class
    + added spaces between encoded chars

---
+++ MorseCode/MorseCode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    class MorseCode
+    {
+        private static readonly Dictionary<string, string> morse = new Dictionary<string, string>()
+        {
+            {"A", ".-"},
+            {"B", "-..."},
+            {"C", "-.-."},
+            {"D", "-.."},
+            {"E", "."},
+            {"F", "..-."},
+            {"G", "--."},
+            {"H", "...."},
+            {"I", ".."},
+            {"J", ".---"},
+            {"K", "-.-"},
+            {"L", ".-.."},
+            {"M", "--"},
+            {"N", "-."},
+            {"O", "---"},
+            {"P", ".--."},
+            {"Q", "--.-"},
+            {"R", ".-."},
+            {"S", "..."},
+            {"T", "-"},
+            {"U", "..-"},
+            {"V", "...-"},
+            {"W", ".--"},
+            {"X", "-..-"},
+            {"Y", "-.--"},
+            {"Z", "--.."},
+            {"0", "-----"},
+            {"1", ".----"},
+            {"2", "..---"},
+            {"3", "...--"},
+            {"4", "....-"},
+            {"5", "....."},
+            {"6", "-...."},
+            {"7", "--..."},
+            {"8", "---.."},
+            {"9", "----."},
+        };
+
+        public string EncodeChar(char letter)
+        {
+            string letterAsString = letter.ToString();
+            if (morse.Keys.Any(key => key.Equals(letterAsString, StringComparison.OrdinalIgnoreCase)))
+            {
+                return morse[letterAsString.ToUpperInvariant()];
+            }
+
+            return null;
+        }
+
+
+        public string Encode(string inputPhrase)
+        {
+            string encodedPhrase = "";
+            foreach (char letter in inputPhrase)
+            {
+                encodedPhrase = encodedPhrase + ' ' + EncodeChar(letter);
+            }
+
+            return encodedPhrase;
+        }
+    }
+}
--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -8,62 +8,18 @@ namespace MorseCode
 {
     class Program
     {
-        private static readonly Dictionary<string, string> morse = new Dictionary<string, string>() {
-                {"A", ".-"},
-                {"B", "-..."},
-                {"C", "-.-."},
-                {"D", "-.."},
-                {"E", "."},
-                {"F", "..-."},
-                {"G", "--."},
-                {"H", "...."},
-                {"I", ".."},
-                {"J", ".---"},
-                {"K", "-.-"},
-                {"L", ".-.."},
-                {"M", "--"},
-                {"N", "-."},
-                {"O", "---"},
-                {"P", ".--."},
-                {"Q", "--.-"},
-                {"R", ".-."},
-                {"S", "..."},
-                {"T", "-"},
-                {"U", "..-"},
-                {"V", "...-"},
-                {"W", ".--"},
-                {"X", "-..-"},
-                {"Y", "-.--"},
-                {"Z", "--.."},
-                {"0", "-----"},
-                {"1", ".----"},
-                {"2", "..---"},
-                {"3", "...--"},
-                {"4", "....-"},
-                {"5", "....."},
-                {"6", "-...."},
-                {"7", "--..."},
-                {"8", "---.."},
-                {"9", "----."},
-            };
-
         static void Main(string[] args)
         {
             Console.WriteLine("**** Fun with Morse Code ****");
 
             Console.Write("Input: ");
-            string input = Console.ReadLine();
+            string inputPhase = Console.ReadLine();
             Console.WriteLine();
             Console.Write("Output: ");           
 
-            foreach (char c in input)
-            {
-                string letter = c.ToString();
-                if (morse.Keys.Any(key => key.Equals(letter, StringComparison.OrdinalIgnoreCase)))
-                {
-                    Console.Write(morse[letter.ToUpperInvariant()]);
-                }
-            }
+            MorseCode m = new MorseCode();
+            Console.WriteLine(m.Encode(inputPhase));
+
             Console.WriteLine();
             Console.ReadLine();
 
