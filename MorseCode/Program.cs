c36932129dc794747358fd63830e9d8970f7fff5
Unknown <iliaterzi15@gmail.com>
2018-03-11 13:45:52 +0100

    moved morse dictionary to outer scope + added lower case char recognition

--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -8,62 +8,60 @@ namespace MorseCode
 {
     class Program
     {
+        private static Dictionary<string, string> morse = new Dictionary<string, string>() {
+                {"A", ".-"},
+                {"B", "-..."},
+                {"C", "-.-."},
+                {"D", "-.."},
+                {"E", "."},
+                {"F", "..-."},
+                {"G", "--."},
+                {"H", "...."},
+                {"I", ".."},
+                {"J", ".---"},
+                {"K", "-.-"},
+                {"L", ".-.."},
+                {"M", "--"},
+                {"N", "-."},
+                {"O", "---"},
+                {"P", ".--."},
+                {"Q", "--.-"},
+                {"R", ".-."},
+                {"S", "..."},
+                {"T", "-"},
+                {"U", "..-"},
+                {"V", "...-"},
+                {"W", ".--"},
+                {"X", "-..-"},
+                {"Y", "-.--"},
+                {"Z", "--.."},
+                {"0", "-----"},
+                {"1", ".----"},
+                {"2", "..---"},
+                {"3", "...--"},
+                {"4", "....-"},
+                {"5", "....."},
+                {"6", "-...."},
+                {"7", "--..."},
+                {"8", "---.."},
+                {"9", "----."},
+            };
+
         static void Main(string[] args)
         {
-
             Console.WriteLine("**** Fun with Morse Code ****");
 
             Console.Write("Input: ");
-            string input = Console.ReadLine().ToUpper();
+            string input = Console.ReadLine();
             Console.WriteLine();
-            Console.Write("Output: ");
-
-            Dictionary<char, string> morse = new Dictionary<char, string>() {
-                {'A', ".-"},
-                {'B', "-..."},
-                {'C', "-.-."},
-                {'D', "-.."},
-                {'E', "."},
-                {'F', "..-."},
-                {'G', "--."},
-                {'H', "...."},
-                {'I', ".."},
-                {'J', ".---"},
-                {'K', "-.-"},
-                {'L', ".-.."},
-                {'M', "--"},
-                {'N', "-."},
-                {'O', "---"},
-                {'P', ".--."},
-                {'Q', "--.-"},
-                {'R', ".-."},
-                {'S', "..."},
-                {'T', "-"},
-                {'U', "..-"},
-                {'V', "...-"},
-                {'W', ".--"},
-                {'X', "-..-"},
-                {'Y', "-.--"},
-                {'Z', "--.."},
-                {'0', "-----"},
-                {'1', ".----"},
-                {'2', "..---"},
-                {'3', "...--"},
-                {'4', "....-"},
-                {'5', "....."},
-                {'6', "-...."},
-                {'7', "--..."},
-                {'8', "---.."},
-                {'9', "----."},
-            };
+            Console.Write("Output: ");           
 
             for (int i = 0; i < input.Length; i++)
-            {
-                
-                char c = input[i];
-                if (morse.ContainsKey(c))
+            {                
+                string c = input[i].ToString();
+                if (morse.Keys.Any(key => key.Equals(c, StringComparison.OrdinalIgnoreCase)))
                 {
-                    Console.Write(morse[c]);
+                    Console.Write(morse[c.ToUpperInvariant()]);
                 }
             }
             Console.WriteLine();
